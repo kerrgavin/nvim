28c21b1703f9dde248bbf0e9143334fba2f7678e
kerrgavin <gnkerr97@gmail.com>
2024-07-01 20:08:21 -0500

    various updates

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -6,9 +6,17 @@ return require('lazy').setup({
 		}
 	},
 	{'Mofiqul/dracula.nvim', as = 'dracula'},
+	{ 'catppuccin/nvim', name = 'catppuccin', priority = 1000 },
 	{
 		'nvim-treesitter/nvim-treesitter',
 		build = ":TSUpdate"
+	},
+	{
+		'folke/tokyonight.nvim',
+		name = 'tokyonight',
+		lazy = false,
+		priority = 1000,
+		opts = {}
 	},
 	{
 		'nvim-telescope/telescope.nvim', version = '0.1.4',
@@ -18,7 +26,12 @@ return require('lazy').setup({
 		'nvim-lualine/lualine.nvim',
 		dependencies = { 'kyazdani42/nvim-web-devicons', lazy = true }
 	},
-	'gennaro-tedesco/nvim-jqx',
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		ft = { "markdown" },
+		build = function() vim.fn["mkdp#util#install"]() end,
+	},
 	{
 		'VonHeikemen/lsp-zero.nvim',
 		branch = 'v2.x',
@@ -37,6 +50,23 @@ return require('lazy').setup({
 			{'saadparwaiz1/cmp_luasnip'},
 			{'rafamadriz/friendly-snippets'},
 		}
+	},
+	{
+		"NeogitOrg/neogit",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"sindrets/diffview.nvim",
+			"nvim-telescope/telescope.nvim"
+		},
+		config = true
+	},
+	{
+		'numToStr/Comment.nvim',
+		lazy = false,
+	},
+	{
+		'folke/trouble.nvim',
+		dependencies = { "nvim-tree/nvim-web-devicons" },
 	}
 },
 {
--- lua/tree-sitter.lua
+++ lua/tree-sitter.lua
@@ -1,5 +1,5 @@
 require 'nvim-treesitter.configs'.setup {
-	ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "json", "java", "yaml", "bash", "rust", "go", "html" },
+	ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "json", "java", "yaml", "bash", "rust", "go", "html", "awk", "latex" },
 	sync_install = false,
 	auto_install = false,
 	highlight = {
